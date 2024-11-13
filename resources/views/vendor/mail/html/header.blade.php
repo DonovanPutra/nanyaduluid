d76419cef58e2a94395e21fbe7ce4bfb1b6153b5
unknown <rafael.saputro@binus.ac.id>
2024-11-13 16:15:12 +0700

    update minor

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://localhost/public/img/logo/semboyan.jpeg" class="logo" alt="Semboyan Logo">
+img src="{{ asset('img/logo/logo.png') }}" class="logo" alt="Logo">
 @else
 {{ $slot }}
 @endif
